8913de5040aabace9b7b353a524942b9b984d370
Phillip Hecker <philliphecker@hotmail.com>
2021-12-04 13:19:42 +0100

    addet ui

--- public/main.js
+++ public/main.js
@@ -1,19 +1,50 @@
 let sceneLoader = new SceneLoader([],document.body)
+//debuging variable
+let sceneIndex = 0;
 
 class MainScene extends Scene{
     //add html here
-    dom = "";
+    dom = '<div class="mainMenue-wrapper">'+
+    '<h1 class="title">Es War Einmal</h1>'+
+    '<button onclick="play()" class="mainMenue-button">Play</button>'+
+    '</div>';
+
     //scene code
     main(){
         
     }
 }
 
+class LvlScene extends Scene{
+    dom = '<div class="lvlMenue-wrapper">'+
+    '<div class="lvlMenue-header">'+
+    '<p class="material-icons" onclick="goBack()">exit_to_app</p>'+
+    '</div>'+
+    '</div>';
+
+    main(){
+
+    }
+}
+
 window.onload = ()=>{
-    let scenes = [];
-    scenes.push(new MainScene());
+    //add scenes here
+    let scenes = [new MainScene(), new LvlScene()];
+
     wrapper = document.getElementById("wrapper");
     sceneLoader = new SceneLoader(scenes,wrapper);
     
+    sceneLoader.load(sceneIndex);
+}
+
+function play(){
+    sceneLoader.load(1);
+}
+
+function goBack(){
     sceneLoader.load(0);
+}
+
+function pickLvl(id){
+    screen.load(id + 2)
 }
